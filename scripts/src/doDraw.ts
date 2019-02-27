dbd8078b1b8e5a38b42c1bb183452c37a799a971
OroshiX <oroshix@users.noreply.github.com>
2019-02-27 23:06:51 +0100

    Convert to JSON (and download file)

--- scripts/src/doDraw.ts
+++ scripts/src/doDraw.ts
@@ -3,22 +3,48 @@ import {Styles} from "./Styles";
 import {PentaWarningError} from "./PentaWarningError";
 import {Serializer} from "./serializer";
 import {PentaChecker} from "./PentaChecker";
-
-let _height = 500;
-let _width = 1000;
+import {PentaJson} from "./pentaJson";
 window.onload = function () {
     let textEditPenta = document.getElementById("text-penta") as HTMLTextAreaElement;
+    let buttonVisualise = document.getElementById("btn-visualise") as HTMLButtonElement;
     let buttonConvert = document.getElementById("btn-convert") as HTMLButtonElement;
+
+    let inputDifficulty = document.getElementById("difficulty-input") as HTMLInputElement;
+    let inputTitle = document.getElementById("title-input") as HTMLInputElement;
     let errors: Node = document.getElementById("errors");
     let warnings: Node = document.getElementById("warnings");
     let success: Node = document.getElementById("success");
     let canvasElement = document.getElementById("penta-representation") as HTMLCanvasElement;
     let context2D: CanvasRenderingContext2D = canvasElement.getContext("2d");
 
-    let ground = new Playground(canvasElement, context2D, new Styles(20, 20));
-    buttonConvert.onclick = () => {
+    function visualise(): PentaWarningError {
         let penta: PentaWarningError = Serializer.serialize(textEditPenta.value);
+        penta.penta.difficulty = +inputDifficulty.value;
+        penta.penta.filename = inputTitle.value;
         PentaChecker.check(penta);
         ground.drawPentatonic(penta, errors, warnings, success);
-    };
+        return penta;
+    }
+
+    function download(filename: string, text: string) {
+        let element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+    }
+    let ground = new Playground(canvasElement, context2D, new Styles(20, 20));
+    buttonVisualise.onclick = visualise;
+    buttonConvert.onclick = () => {
+        let penta = visualise();
+        let pentaJson = PentaJson.fromPentatonic(penta.penta);
+        let jsonString = JSON.stringify(pentaJson);
+        download(pentaJson.name + ".json", jsonString);
+    }
+
 };
--- scripts/src/pentaJson.ts
+++ scripts/src/pentaJson.ts
@@ -1,6 +1,7 @@
 import {Value} from "./value";
 import {Sister} from "./sister";
 import {Diff} from "./diff";
+import {Pentatonic} from "./Pentatonic";
 
 export class PentaJson {
     author: String;
@@ -23,6 +24,24 @@ export class PentaJson {
         this.data = [];
         this.differences = [];
         this.sisters = [];
+    }
+
+    static fromPentatonic(penta: Pentatonic): PentaJson {
+        let res = new PentaJson();
+        res.author = penta.author;
+        res.difficulty = penta.difficulty;
+        res.name = penta.filename;
+        res.width = penta.columns;
+        res.height = penta.lines;
+        res.differences = penta.diffOnes;
+        res.sisters = penta.sisters;
+
+        res.data = penta.cells.map(row => row.map(cell => cell.area.id));
+        res.values = penta.cellArray()
+            .filter(cell => cell.values.length == 1)
+            .map(cell => new Value(+cell.values[0], cell.position.i, cell.position.j));
+
+        return res;
     }
 }
 
