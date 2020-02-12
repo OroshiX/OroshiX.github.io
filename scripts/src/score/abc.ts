accdde09967e5f59820556d331e86de2c698ee99
OroshiX <oroshix@users.noreply.github.com>
2020-02-12 17:56:29 +0100

    Added midi, and score format

--- scripts/src/score/abc.ts
+++
@@ -1,6 +0,0 @@
-const abcjs: any = require('abcjs');
-window.onload = function () {
-    let tune = document.getElementById("tune") as HTMLDivElement;
-
-    abcjs.renderAbc("paper",tune.innerText,{});
-};
